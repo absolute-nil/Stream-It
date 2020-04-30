f0f3a3a46ff8bedb291ce4e850094868a7b727ac
Your Name <nikhil19@somaiya.edu>
2020-04-30 14:27:18 +0530

    add redux form to create stream

--- client/src/index.js
+++ client/src/index.js
@@ -11,8 +11,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store = createStore(
   reducers,
-
-  composeEnhancers
+  composeEnhancers(applyMiddleware())
   )
 
 ReactDOM.render(
