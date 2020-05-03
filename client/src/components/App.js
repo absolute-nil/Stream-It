165d9f3dadeb51998bebdcb690636730ff39458a
Your Name <nikhil19@somaiya.edu>
2020-05-03 16:53:23 +0530

    implement streaming with nms and flv.json

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,29 +1,31 @@
-import React from 'react'
-import {Router, Route} from "react-router-dom"
+import React from "react";
+import { Router, Route, Switch } from "react-router-dom";
 
-import StreamCreate from "./streams/StreamCreate"
-import StreamEdit from "./streams/StreamEdit"
-import StreamDelete from "./streams/StreamDelete"
-import StreamList from "./streams/StreamList"
-import StreamShow from "./streams/StreamShow"
-import Header from "./Header"
-import history from "../history"
+import StreamCreate from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
+import Header from "./Header";
+import history from "../history";
 
 const App = () => {
-    return (
-        <div className="ui container">
-        <Router history={history}>
-            <div>
-                <Header />
-                <Route path="/" exact component={StreamList} />
-                <Route path="/streams/new" exact component={StreamCreate} />
-                <Route path="/streams/edit/:id" exact component={StreamEdit} />
-                <Route path="/streams/delete/:id" exact component={StreamDelete} />
-                <Route path="/streams/show/:id" exact component={StreamShow} />
-            </div>
-        </Router>
+  return (
+    <div className="ui container">
+      <Router history={history}>
+        <div>
+          <Header />
+          <Switch>
+            <Route path="/" exact component={StreamList} />
+            <Route path="/streams/new" exact component={StreamCreate} />
+            <Route path="/streams/edit/:id" exact component={StreamEdit} />
+            <Route path="/streams/delete/:id" exact component={StreamDelete} />
+            <Route path="/streams/:id" exact component={StreamShow} />
+          </Switch>
         </div>
-    )
-}
+      </Router>
+    </div>
+  );
+};
 
-export default App
+export default App;
